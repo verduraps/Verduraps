5d5f0c56eedfe8ac0371703ef75503546e787e59
verduraps <aeaoaeo@gmail.com>
2026-04-28 20:42:18 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'verdapp-v20260428-carteles';
+const CACHE_NAME = 'verdapp-v11';
 const APP_SHELL = ['./','./index.html?v=20260428-carteles','./manifest.json','./icons/icon.svg'];
 self.addEventListener('install', event => { self.skipWaiting(); event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL).catch(() => null))); });
 self.addEventListener('activate', event => { event.waitUntil(caches.keys().then(keys => Promise.all(keys.map(key => key !== CACHE_NAME ? caches.delete(key) : null))).then(() => self.clients.claim())); });
