5459f4894fca131357f28a6f04509158260fa4d8
verduraps <aeaoaeo@gmail.com>
2026-04-28 13:42:07 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'verdapp-v9';
+const CACHE = 'verdapp-v10';
 const ASSETS = ['./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install', e => {
